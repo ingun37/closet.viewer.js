083e4920b9b480fe9befa2c6041c1c407be50d02
Tkay KIM <53590273+CLO-Tkay@users.noreply.github.com>
2020-03-13 16:45:48 +0900

    setWireframe returns whether it succeeded or not (#14)
    Co-authored-by: Tkay KIM <t4ehye0ng@gmail.com>

--- src/lib/clo/readers/ZrestLoader.js
+++ src/lib/clo/readers/ZrestLoader.js
@@ -137,10 +137,20 @@ ZRestLoader.prototype = {
     // NOTE: This function designed to set wireframe for several types of meshes.
     //       But for now, works for pattern meshes only.
 
-    this.zProperty.drawMode.wireframe.pattern = bWireframe;
-    this.matMeshMap.forEach(matMesh => {
-      matMesh.material.wireframe = bWireframe;
-    });
+    try { 
+      this.zProperty.drawMode.wireframe.pattern = bWireframe;
+      this.matMeshMap.forEach(matMesh => {
+        if (typeof matMesh.material === 'undefined' || typeof matMesh.material.wireframe === 'undefined') {
+          throw BreakException;
+        }
+        matMesh.material.wireframe = bWireframe;
+      });
+      
+      return true;
+    }
+    catch (e) {
+      return false;
+    }
   },
 
   parse(data, onLoad) {
