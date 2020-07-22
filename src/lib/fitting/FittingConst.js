a6b67df2f044c8f0cd76830c92458640b51c1fc9
Tkay KIM <tkay@clo3d.com>
2020-07-22 16:09:37 +0900

    Split define const variables to the file

---
+++ src/lib/fitting/FittingConst.js
@@ -0,0 +1,80 @@
+export const MEASUREMENT_LIST_NAME = {
+  // Base
+  HEIGHT_Total: 0,
+  WEIGHT_Total: 1,
+
+  // Circumference
+  CIRCUMFERENCE_Bust: 2,
+  CIRCUMFERENCE_MidNeck: 3,
+  CIRCUMFERENCE_NeckBase: 4,
+  CIRCUMFERENCE_UnderBust: 5,
+  CIRCUMFERENCE_Waist: 6,
+  CIRCUMFERENCE_HighHip: 7,
+  CIRCUMFERENCE_LowHip: 8,
+  CIRCUMFERENCE_Thigh: 9,
+  CIRCUMFERENCE_MidThigh: 10,
+  CIRCUMFERENCE_Knee: 11,
+  CIRCUMFERENCE_Calf: 12,
+  CIRCUMFERENCE_Ankle: 13,
+  CIRCUMFERENCE_Armhole: 14,
+  CIRCUMFERENCE_Bicep: 15,
+  CIRCUMFERENCE_Elbow: 16,
+  CIRCUMFERENCE_Wrist: 17,
+
+  // Height
+  HEIGHT_HPS: 18,
+  HEIGHT_Shoulder: 19,
+  HEIGHT_ShoulderDrop: 20,
+  HEIGHT_APEX: 21,
+  HEIGHT_UnderBust: 22,
+  HEIGHT_Waist: 23,
+  HEIGHT_HighHip: 24,
+  HEIGHT_LowHip: 25,
+  HEIGHT_Crotch: 26,
+  HEIGHT_Thigh: 27,
+  HEIGHT_MidThigh: 28,
+  HEIGHT_Knee: 29,
+  HEIGHT_Calf: 30,
+  HEIGHT_Ankle: 31,
+
+  // Length
+  LENGTH_Bust_Width: 32,
+  LENGTH_Bust_Depth: 33,
+  LENGTH_AcrossFront: 34,
+  LENGTH_AcrossBack: 35,
+  LENGTH_AcrossShoulder: 36,
+  LENGTH_CBNeck_Shoulder: 37, // always deactivate
+  LENGTH_CFNeck_AcrossFront: 38,
+  LENGTH_CBNeck_AcrossBack: 39,
+  LENGTH_CFNeck_Waist: 40,
+  LENGTH_CBNeck_Waist: 41,
+  LENGTH_Waist_HighHip: 42,
+  LENGTH_Waist_LowHip: 43,
+  LENGTH_APEX_APEX: 44,
+  LENGTH_HPS_APEX: 45,
+  LENGTH_Halter: 46, // always deactivate
+  LENGTH_Arm: 47,
+  LENGTH_CBNeck_Wrist: 48,
+  LENGTH_Total_Rise: 49,
+  LENGTH_Front_Rise: 50, // always deactivate
+  LENGTH_Back_Rise: 51, // always deactivate
+  LENGTH_CrotchDepth: 52, // always deactivate
+  LENGTH_CrotchWidth: 53, // always deactivate
+  LENGTH_Crothch_MidThigh: 54, // always deactivate
+  LENGTH_VerticalTrunk_Front: 55, // always deactivate
+  LENGTH_VerticalTrunk_Back: 56, // always deactivate
+  LENGTH_VerticalTrunk: 57, // always deactivate
+
+  // End
+  SIZE_OF_MEASUREMENT_LIST: 58,
+};
+
+export const AVATAR_GENDER = {
+  GENDER_MALE: 0,
+  GENDER_FEMALE: 1,
+  GENDER_BOY: 2,
+  GENDER_GIRL: 3,
+  GENDER_PRESCHOOL: 4,
+  GENDER_NONE: 5,
+  GENDER_SIZE: 6,
+};
