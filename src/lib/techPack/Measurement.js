b3fdb954b5c632c22f87f40c48d268c882bd42b4
Tkay KIM <53590273+CLO-Tkay@users.noreply.github.com>
2020-07-28 09:52:38 +0900

    Hotfix/measurement info missing (#41)
    * Add defense code for measurement info
    
    * Update version

--- src/lib/techPack/Measurement.js
+++ src/lib/techPack/Measurement.js
@@ -17,12 +17,15 @@ export class Measurement {
     this.matMeshMap = matMeshMap;
 
     let geometryIndex = 0;
-    listPatternMeasure.forEach(entry => {
+    listPatternMeasure.forEach((entry) => {
       const arrPos = entry.get("arrPosition3D");
+      if (!arrPos) {
+        return;
+      }
 
       // Gather points for measure lines
       const points = [];
-      arrPos.forEach(pos => {
+      arrPos.forEach((pos) => {
         points.push(new THREE.Vector3(pos.x, pos.y, pos.z));
       });
 
@@ -49,7 +52,7 @@ export class Measurement {
   }
 
   setAllVisible(bVisible) {
-    this.geometryMap.forEach(line => {
+    this.geometryMap.forEach((line) => {
       line.visible = bVisible;
     });
   }
@@ -64,7 +67,7 @@ export class Measurement {
   }
 
   adjustAllMeshOffset(bOffset, offset = 100) {
-    this.matMeshMap.forEach(matMesh => {
+    this.matMeshMap.forEach((matMesh) => {
       const material = matMesh.material;
       if (material) {
         material.polygonOffset = bOffset;
@@ -72,5 +75,5 @@ export class Measurement {
         material.polygonOffsetUnits = offset;
       }
     });
-  };
+  }
 }
