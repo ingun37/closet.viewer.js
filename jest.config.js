04e830a3a6b85546f08e98261f79b4499e609193
Ingun <ingun37@gmail.com>
2020-09-05 01:08:53 +0900

    Typescript, Unit Test suppport apply ts-loader, jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    "^@bg(.*)$": "<rootDir>/src/lib/clo/background$1",
+  },
+  transform: {
+    "\\.(jpg|jpeg|png)$": "<rootDir>/__mocks__/image2DataURL.js",
+  }
+};
---
+++ webpack.localhost-config.js
@@ -0,0 +1,5 @@
+
+var base = require('./webpack.config');
+
+base.devServer.host = "localhost"
+module.exports = base;
