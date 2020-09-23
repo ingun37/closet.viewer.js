e7179a0c777252cf03a6044474bc8dafea52546f
Ingun <ingun37@gmail.com>
2020-09-23 21:56:36 +0900

    to be squashed

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,7 @@ module.exports = {
       {
         test: /\.ts?$/,
         use: 'ts-loader',
-        include: resolve('src'),
-        exclude: [/\.spec\.ts$/]
+        include: resolve('src')
       },
       {
         test: /\.js$/,
