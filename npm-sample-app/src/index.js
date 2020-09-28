aada2bdfb05758a45e9410b55f87f94f16998b9c
Ingun Jon <ingun@foxxing.com>
2020-09-28 11:41:17 +0900

    npm sample app

---
+++ npm-sample-app/src/index.js
@@ -0,0 +1,5 @@
+import ClosetViewer from "closet-viewer";
+export {default as ClosetViewer} from "closet-viewer";
+console.log("exporting viewer");
+export var viewer = new ClosetViewer();
+console.log("done!s");
