8365298c3baf47ca6df2ae4dffe0f22e35340654
Ingun Jon <ingun@foxxing.com>
2020-09-29 15:27:45 +0900

    refactory test code using describe.each

--- gl-test/test.spec.ts
+++ gl-test/test.spec.ts
@@ -6,41 +6,43 @@ import { hash } from "imghash";
 import leven from "leven";
 import puppeteer from "puppeteer";
 
+// Consider using jest-puppeteer
+
 const casesPath = PATH.join(__dirname, 'cases')
 
-describe('graphic', () => {
-    var browser:puppeteer.Browser;
-    beforeAll(async ()=>{
-        browser = await puppeteer.launch();
-    })
-    afterAll(async ()=>{
-        await browser.close();
-    })
-    fs.readdirSync(casesPath).map(x => PATH.join(casesPath, x)).filter(x => fs.statSync(x).isDirectory()).forEach(casePath => {
+const testCases = fs.readdirSync(casesPath).map(x => PATH.join(casesPath, x)).filter(x => fs.statSync(x).isDirectory())
 
-        const htmlFilePath = PATH.join(casePath, 'scenario.html');
-        const htmlExists = fs.existsSync(htmlFilePath);
-        test(`${htmlFilePath} exists`, () => expect(htmlExists).toBeTruthy());
+var browser: puppeteer.Browser;
+beforeAll(async () => {
+    browser = await puppeteer.launch();
+})
+afterAll(async () => {
+    await browser.close();
+})
 
-        const expectPath = PATH.join(casePath, 'expect.png');
-        const expectExists = fs.existsSync(expectPath);
-        test(`${expectPath} exists`, () => expect(expectExists).toBeTruthy())
+describe.each(testCases)("graphic", (casePath:string) => {
+    const htmlFilePath = PATH.join(casePath, 'scenario.html');
+    const htmlExists = fs.existsSync(htmlFilePath);
+    test(`${htmlFilePath} exists`, () => expect(htmlExists).toBeTruthy());
 
-        if (htmlExists && expectExists) {
-            test(`${PATH.basename(casePath)} renders as expected`, async () => {
-                const page = await browser.newPage();
-                const htmlFileURL = URL.pathToFileURL(htmlFilePath);
-                await page.goto(htmlFileURL.toString());
-                await page.waitForSelector("#finish");
-                const element = await page.$("canvas");
-                const pngBase64 = await element.screenshot();
-                const sampleImgPath = PATH.join(casePath, "result.png");
-                fs.writeFileSync(sampleImgPath, pngBase64, { encoding: 'base64' });
-                const difference = await calculateDifference(sampleImgPath, expectPath);
-                expect(difference).toBeLessThan(1);
-            })
-        }
-    })
+    const expectPath = PATH.join(casePath, 'expect.png');
+    const expectExists = fs.existsSync(expectPath);
+    test(`${expectPath} exists`, () => expect(expectExists).toBeTruthy())
+
+    if (htmlExists && expectExists) {
+        test(`${PATH.basename(casePath)} renders as expected`, async () => {
+            const page = await browser.newPage();
+            const htmlFileURL = URL.pathToFileURL(htmlFilePath);
+            await page.goto(htmlFileURL.toString());
+            await page.waitForSelector("#finish");
+            const element = await page.$("canvas");
+            const pngBase64 = await element.screenshot();
+            const sampleImgPath = PATH.join(casePath, "result.png");
+            fs.writeFileSync(sampleImgPath, pngBase64, { encoding: 'base64' });
+            const difference = await calculateDifference(sampleImgPath, expectPath);
+            expect(difference).toBeLessThan(1);
+        })
+    }
 })
 
 async function calculateDifference(sampleImagePath: string, expectedImagePath: string) {
